f6a1023136b5c3376b4e1c9bda9c22c8fe18cba1
DiFve <jkblade6@gmail.com>
2021-04-15 18:32:26 +0700

    Draw Card

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,27 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class Controller {
-    public void btn1OnAction(ActionEvent event)
+    Deck deck = new Deck();
+    int total=0;
+    @FXML
+    private Label currentNum;
+    public void useSpecialBtnOnAction(ActionEvent event)
     {
-        System.out.println("LALALALALA");
+        System.out.println("Special");
+    }
+
+    public void keepCurrentBtnOnAction(ActionEvent event)
+    {
+        System.out.println("Keep");
+    }
+
+    public void drawCardBtnOnAction(ActionEvent event)
+    {
+        total+=deck.draw();
+        currentNum.setText(total+"/21");
     }
 }
---
+++ src/sample/Deck.java
@@ -0,0 +1,44 @@
+package sample;
+
+import java.util.Random;
+
+public class Deck {
+    private int[] cardDeck = {1,1,1,1,1,1,1,1,1,1,1};
+    static private int count=0;
+    public Deck() {
+
+    }
+    public int draw()
+    {
+        int rand;
+
+        Random random = new Random();
+        int specialCardChance = random.nextInt(12); // 1 in 12
+        if(specialCardChance==0)
+        {
+            System.out.println("Got a Special");
+        }
+        if(count<11)
+        {
+            System.out.println("Card");
+            while(true)
+            {
+                rand = random.nextInt(11);
+                if(cardDeck[rand]==0)
+                    continue;
+                else
+                {
+                    count++;
+                    cardDeck[rand]--;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Out of Deck");
+            return 0;
+        }
+        return rand+1;
+    }
+}
