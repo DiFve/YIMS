96e6c9f0cf4ce1ffa47ec2f44c2cef86c1261ffb
DiFve <jkblade6@gmail.com>
2021-04-16 00:39:06 +0700

    Draw 4

--- src/yimsbean/YIMSBean.java
+++ src/yimsbean/YIMSBean.java
@@ -23,7 +23,6 @@ public class YIMSBean extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         launch(args);
     }
 
